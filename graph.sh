47e9d2aa3dd4f3ca514191a37addc7e599bbdee5
Ali Shafahi <ashafahi@gmail.com>
2019-06-26 12:31:50 -0400

    Both graphs

--- graph.sh
+++ graph.sh
@@ -2,4 +2,6 @@
 #python graph.py -f test_0.xls test_1.xls -l 'Boring' 'Fast' -o test.pdf -d outs/
 #python graph.py -f train_0.xls train_1.xls -l 'Boring' 'Fast' -o train.pdf -d outs/
 python graph.py -f Train_boring.xls Train_fast.xls -l 'Boring' 'Fast' -o train_100 -d .
+python graph.py -f Train_e_boring.xls Train_e_fast.xls -l 'Boring' 'Fast' -o train_e_100 -d .
 python graph.py -f Test_boring.xls Test_fast.xls -l 'Boring' 'Fast' -o Test_100 -d .
+python graph.py -f Test_e_boring.xls Test_e_fast.xls -l 'Boring' 'Fast' -o Test_e_100 -d .
--- tograph.sh
+++ tograph.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 for i in 'boring' 'fast' ; do 
 for j in 'Train' 'Test' ; do 
-	f="${j}_${i}.xls"
+for e in '_e_' '_' ; do 
+	f="${j}${e}${i}.xls"
 	rm ${f};
 done 
+done
 done 
 for w in 'boring' 'fast' ; do 
 	f="${w}.out"
 	len=$(cat "${f}" | grep "Test" | wc -l) 
 	for i in `seq 1 ${len}` ; do 
-		t=$(cat "${f}" | grep "Time" | tail -n "${i}" | head -n 1 | awk '{print $2}')
+		t=$(cat "${f}" | grep "Time" | tail -n "${i}" | head -n 1 | awk '{print $3}')
+		e=$(cat "${f}" | grep "Time" | tail -n "${i}" | head -n 1 | awk '{print $2}')
 		tr=$(cat "${f}" | grep "Train" | tail -n "${i}" | head -n 1 | awk '{print $3}')
 		ts=$(cat "${f}" | grep "Test" | tail -n "${i}" | head -n 1 | awk '{print $3}')
 		echo -e "$t\t$tr\t$ts"
 		echo -e "${t}\t${tr}" >> "Train_${w}.xls"
 		echo -e "${t}\t${ts}" >> "Test_${w}.xls"
+		echo -e "${e}\t${tr}" >> "Train_e_${w}.xls"
+		echo -e "${e}\t${ts}" >> "Test_e_${w}.xls"
 	done
 		
 done
@@ -23,10 +28,11 @@ ls .
 
 for i in 'boring' 'fast' ; do 
 for j in 'Train' 'Test' ; do 
-
-	f="${j}_${i}.xls"
+for e in '_e_' '_' ; do 
+	f="${j}${e}${i}.xls"
 	echo "======+${f}======="
 	cat "${f}"
+done 
 done 
 done 
 source graph.sh
