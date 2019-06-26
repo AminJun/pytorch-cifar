cd19cd08b916e053b247001ac7c718f476f35987
Ali Shafahi <ashafahi@gmail.com>
2019-06-26 11:38:41 -0400

    Saveing stuff

--- analyze.sh
+++ analyze.sh
@@ -3,8 +3,8 @@ while true ; do
 	echo -e "way\tepoch\ttrain\ttest"
 for f in "boring" "fast" ; do 
 	file="${f}.out"
-	train=$(tail "${file}" | head -n 5 | grep "Train" | tail -n 1 | sed 's/.*Acc://g' | sed 's/\%.*//g')
-	ttest=$(tail "${file}" | head -n 5 | grep "Test" | tail -n 1 | sed 's/.*Acc://g' | sed 's/\%.*//g')
+	train=$(tail "${file}" | head -n 5 | grep "Train" | tail -n 1)
+	ttest=$(tail "${file}" | head -n 5 | grep "Test" | tail -n 1)
 	epoch=$(tail "${file}" | grep "Epoch" | awk '{print $2}' | tail -n 1)
 	echo -e "${f}\t${epoch}\t${train}\t${ttest}"
 done
